12b83b876540e2938ab8c8bd806e4d6733ce0d77
verdandi144 <courage144@naver.com>
2021-02-01 00:10:07 +0900

    Final Version

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:plastic_island/request.dart';
 import 'package:tflite/tflite.dart';
 
 void main() {
--- lib/request.dart
+++
@@ -1,6 +0,0 @@
-import 'package:http/http.dart';
-
-Future getData(url) async {
-  Response response = await get(url);
-  return response.body;
-}
